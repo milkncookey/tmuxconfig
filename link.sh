2c9bd2f8ad057e385e1331028902fde90ac34eec
Ruben Wu <ruben.wu@gmail.com>
2018-08-30 13:42:54 -0700

    Fixed bug involving accidentally added .tmux when folder configs doesnt exist yet

--- link.sh
+++ link.sh
@@ -6,7 +6,5 @@ DIR="$(cd "$(dirname "$0")"; pwd -P)"
 
 rm -rf $BACKUP_DIR
 mkdir -p $BACKUP_DIR
-[ -e ~/.tmux ]      && mv ~/.tmux      $BACKUP_DIR
 [ -e ~/.tmux.conf ] && mv ~/.tmux.conf $BACKUP_DIR
-ln -sf $DIR/tmux      ~/.tmux
 ln -sf $DIR/tmux.conf ~/.tmux.conf
