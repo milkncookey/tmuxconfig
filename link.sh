666af9cd34b2aaaae346a2f205f3eed827f9ca2f
Ruben Wu <ruben.wu@gmail.com>
2018-09-08 12:50:53 -0400

    Added checker to attempt to install tmux if the program  is not already installed

--- link.sh
+++ link.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-set -euox pipefail 
+set -euox pipefail
+
+# In case tmux is not installed yet, then why you trying to config
+if ! type "tmux" > /dev/null; then
+    while true; do
+        read -p "Do you wish to install tmux?
+" yn
+        case $yn in
+            Yes ) sudo apt-get install tmux; break;;
+            * ) echo "Continuing without installing tmux"; break;;
+        esac
+    done
+fi
 
 BACKUP_DIR=/tmp/dotconfig_backup/tmuxconfig
 DIR="$(cd "$(dirname "$0")"; pwd -P)"
